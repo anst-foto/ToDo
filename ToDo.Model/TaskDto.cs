a77a03a0c08761255bd5b570cae2a875a1fd0f63
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-23 20:05:00 +0300

    Add Model proj

---
+++ ToDo.Model/TaskDto.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDo.Model;
+
+public enum TaskPriority
+{
+    Important, Whatever
+}
+
+public enum TaskStatus
+{
+    Planned, InProgress, Completed, Paused, OutOfTime
+}
+
+public class TaskDto
+{
+    public uint Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TaskPriority Priority { get; set; } = TaskPriority.Whatever;
+    public TaskStatus Status { get; set; } = TaskStatus.Planned;
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Deadline { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
