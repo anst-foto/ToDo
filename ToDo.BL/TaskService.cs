b9b6bdfaa0b2118169914845c0b66dc3bac2ea72
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-23 21:23:40 +0300

    Добавление DAL в BL

--- ToDo.BL/TaskService.cs
+++ ToDo.BL/TaskService.cs
@@ -1,31 +1,57 @@
-﻿using ToDo.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.DAL;
+using ToDo.Model;
 
 namespace ToDo.BL;
 
 public class TaskService
 {
+    private readonly ToDoContext _context;
+
+    public TaskService()
+    {
+        var contextFactory = new ToDoContextFactory();
+        _context = contextFactory.CreateDbContext();
+    }
+
     public async Task<IEnumerable<TaskDto>> GetAllTasksAsync()
     {
-        throw new NotImplementedException();
+        return await _context.Tasks.ToListAsync();
     }
 
     public async Task<TaskDto?> GetTaskByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _context.Tasks.SingleOrDefaultAsync(t => t.Id == id);
     }
 
     public async Task UpdateTaskAsync(TaskDto task)
     {
-        throw new NotImplementedException();
+        var temp = await _context.Tasks.SingleOrDefaultAsync(t => t.Id == task.Id);
+        if (temp == null) throw new NullReferenceException();
+        //_context.Entry(temp).CurrentValues.SetValues(task);
+        temp.Title = task.Title;
+        temp.Description = task.Description;
+        temp.Created = task.Created;
+        temp.Deadline = task.Deadline;
+        temp.Priority = task.Priority;
+        temp.Status = task.Status;
+        temp.IsDeleted = task.IsDeleted;
+        
+        await _context.SaveChangesAsync();
     }
 
     public async Task DeleteTaskAsync(int id)
     {
-        throw new NotImplementedException();
+        var temp = await _context.Tasks.SingleOrDefaultAsync(t => t.Id == id);
+        if (temp == null) throw new NullReferenceException();
+        
+        temp.IsDeleted = true;
+        await _context.SaveChangesAsync();
     }
 
     public async Task CreateTaskAsync(TaskDto task)
     {
-        throw new NotImplementedException();
+        await _context.Tasks.AddAsync(task);
+        await _context.SaveChangesAsync();
     }
 }
