4d1e88949952024fabe4b4c895fa19b6e96aa0a2
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-23 19:28:24 +0300

    Добавление заглушек для WebAPI

--- ToDo.WebApi/Program.cs
+++ ToDo.WebApi/Program.cs
@@ -2,9 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
-    {
-        
-    });
+app.MapGet("/tasks", () => { });
+app.MapGet("/tasks/{id}", () => { });
+app.MapPost("/tasks", () => {});
+app.MapPut("/tasks/{id}", () => {});
+app.MapDelete("/tasks/{id}", () => {});
 
 app.Run();
