75d61f53a97447fe64ec1f90669731e397e2d21a
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-23 20:10:47 +0300

    Add BL proj

---
+++ ToDo.BL/TaskService.cs
@@ -0,0 +1,31 @@
+﻿using ToDo.Model;
+
+namespace ToDo.BL;
+
+public class TaskService
+{
+    public IEnumerable<TaskDto> GetAllTasks()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TaskDto? GetTaskById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTask(TaskDto task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteTask(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateTask(TaskDto task)
+    {
+        throw new NotImplementedException();
+    }
+}
