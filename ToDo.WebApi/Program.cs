adc971441411b37fe1534205974a69ea4961218a
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-25 19:28:26 +0300

    Add FIXME

--- ToDo.WebApi/Program.cs
+++ ToDo.WebApi/Program.cs
@@ -7,8 +7,8 @@ app.UseHttpsRedirection();
 
 var service = new TaskService();
 
-app.MapGet("/tasks", async () => await service.GetAllTasksAsync());
-app.MapGet("/tasks/{id:int}", async (int id) => await service.GetTaskByIdAsync(id));
+app.MapGet("/tasks", async () => await service.GetAllTasksAsync());//FIXME
+app.MapGet("/tasks/{id:int}", async (int id) => await service.GetTaskByIdAsync(id));//FIXME
 app.MapPost("/tasks", async (TaskDto task) => await service.CreateTaskAsync(task));
 app.MapPut("/tasks", async (TaskDto task) => await service.UpdateTaskAsync(task));
 app.MapDelete("/tasks/{id:int}", async (int id) => await service.DeleteTaskAsync(id));
